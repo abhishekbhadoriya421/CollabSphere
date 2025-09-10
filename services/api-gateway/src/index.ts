82ac75bad4243eed7833b2ac1e7ea69b58579eb3
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-10 23:21:27 +0530

    created first api index and install dependencies

---
+++ services/api-gateway/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).send('API Gateway is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on http://localhost:${PORT}`);
+});
