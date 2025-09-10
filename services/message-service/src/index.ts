567edc8771ccc66821c5065932fd66840e4a65f2
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-10 23:57:01 +0530

    setup message service file index.ts

---
+++ services/message-service/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).send('Message Service is running');
+}
+);
+app.listen(PORT, () => {
+    console.log(`Message Service is running on http://localhost:${PORT}`);
+}
+);
