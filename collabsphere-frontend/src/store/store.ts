a33ee55bf36f8a4e20601c23fea4a263070c8fe9
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-18 22:59:33 +0530

    added reducer in store

--- collabsphere-frontend/src/store/store.ts
+++ collabsphere-frontend/src/store/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import RegistrationReducer from "../features/AuthenticationSlice/RegistrationSlice";
 import LoginReducer from '../features/AuthenticationSlice/LoginSlice';
-
+import ActivityItemReducer from '../features/ServicesSlice/ActivityItemSlice';
 
 export const store = configureStore({
     reducer: {
         RegistrationReducer: RegistrationReducer,
-        LoginReducer: LoginReducer
+        LoginReducer: LoginReducer,
+        ActivityItemReducer: ActivityItemReducer
     },
 });
 
