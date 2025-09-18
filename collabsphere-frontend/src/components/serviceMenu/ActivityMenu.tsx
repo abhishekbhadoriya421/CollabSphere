de473749d2909883cec8a54d19d1af56e5197fbc
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-18 21:58:48 +0530

    updated design

--- collabsphere-frontend/src/components/serviceMenu/ActivityMenu.tsx
+++ collabsphere-frontend/src/components/serviceMenu/ActivityMenu.tsx
@@ -6,7 +6,7 @@ type ActivityMenuProps = {
 
 export default function ActivityMenu({ content, icon, isActive }: ActivityMenuProps) {
     return (
-        <div className={`flex h-1/4 items-center 
+        <div className={`flex h-1/4 items-center cursor-pointer hover:bg-[#1b2431]
         ${isActive ? " border-l-6 border-l-green-400 bg-[#4f46e5]" : null}`}>
             <p className="ml-6"><i className={icon} ></i></p>
             <p className="ml-3">{content}</p>
