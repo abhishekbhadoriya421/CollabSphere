9eb61a7392fac53b38110212724ba3ba5a9c66e9
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-18 21:52:18 +0530

    design activity menu

--- collabsphere-frontend/src/components/serviceMenu/ServiceMenu.tsx
+++ collabsphere-frontend/src/components/serviceMenu/ServiceMenu.tsx
@@ -1,11 +1,45 @@
+import ActivityMenu from "./ActivityMenu";
 import ServiceMenuHeader from "./ServiceMenuHeader";
 
 export default function ServiceMenu() {
+
+    const activityItems = [
+        {
+            icon: 'fas fa-comment',
+            content: 'Chat',
+            isActive: true
+        },
+        {
+            icon: 'fas fa-phone',
+            content: 'Calls',
+            isActive: false
+        },
+        {
+            icon: 'fas fa-video',
+            content: 'Meetings',
+            isActive: false
+        },
+        {
+            icon: 'fas fa-file',
+            content: 'Files',
+            isActive: false
+        }
+    ];
     return (
         <div className="w-[100%] h-screen text-white">
-            <div className="h-1/8 border-b">
+            <div className="h-1/8">
                 <ServiceMenuHeader />
             </div>
+            <hr />
+            <div className="h-3/9">
+                {
+                    activityItems.map((activity, index) => (
+                        <ActivityMenu key={index} content={activity.content} icon={activity.icon} isActive={activity.isActive} />
+                    ))
+                }
+
+            </div>
+            <hr />
         </div>
     );
 }
