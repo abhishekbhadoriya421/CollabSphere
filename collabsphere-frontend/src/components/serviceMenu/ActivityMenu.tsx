8b49591dbf50188327f3d0a74cd1ea91ea3d790b
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-21 00:26:58 +0530

    updated

--- collabsphere-frontend/src/components/serviceMenu/ActivityMenu.tsx
+++ collabsphere-frontend/src/components/serviceMenu/ActivityMenu.tsx
@@ -8,8 +8,8 @@ type ActivityMenuProps = {
 
 export default function ActivityMenu({ id, content, icon, isActive, handleActiveActivity }: ActivityMenuProps) {
     return (
-        <div onClick={() => handleActiveActivity(id)} className={`flex h-1/4 items-center cursor-pointer hover:bg-[#1b2431]
-        ${isActive === 'ACTIVE' ? " border-l-6 border-l-green-400 bg-[#4f46e5]" : null}`}>
+        <div onClick={() => handleActiveActivity(id)} className={`flex h-1/4 items-center cursor-pointer 
+        ${isActive === 'ACTIVE' ? " border-l-6 border-l-green-400 bg-[#4f46e5] hover:bg-[#3a34ae]" : "hover:bg-[#1b2431]"}`}>
             <p className="ml-6"><i className={icon} ></i></p>
             <p className="ml-3">{content}</p>
         </div>
