fbab6df186735633fb30b735aac4e6c7e84d805a
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-20 22:03:29 +0530

    wrap up actions in try catch

--- services/api-gateway/src/controller/AuthenticationController.ts
+++ services/api-gateway/src/controller/AuthenticationController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from '../models/User';
 import { Authentication } from "../service/JWT_Authentication";
+import ErrorHandler from "../utils/ErrorHandler";
 /**
  * Validate User email and password if validation is passed return access token and set refresh token in cookie 
  *  return {
@@ -11,80 +12,90 @@ import { Authentication } from "../service/JWT_Authentication";
     }
  */
 export const LoginAction = async (req: Request, res: Response) => {
-    interface LoginRequest {
-        email: string;
-        password: string;
-    }
-    interface LoginResponse {
-        status: number | 400;
-        message: string | 'Invalid request';
-        token: string | null;
-        user: object | null;
-    }
-    const { email, password }: LoginRequest = req.body;
-    if (!email || !password) {
-        const response: LoginResponse = {
-            status: 400,
-            message: 'Email and password are required',
-            token: null,
-            user: null
-        };
-        return res.status(400).json(response);
-    }
-    interface UserAuthenticationResponse {
-        status: boolean;
-        user: User | null;
-        message: string;
-    }
+    try {
+        interface LoginRequest {
+            email: string;
+            password: string;
+        }
+        interface LoginResponse {
+            status: number | 400;
+            message: string | 'Invalid request';
+            token: string | null;
+            user: object | null;
+        }
+        const { email, password }: LoginRequest = req.body;
+        if (!email || !password) {
+            const response: LoginResponse = {
+                status: 400,
+                message: 'Email and password are required',
+                token: null,
+                user: null
+            };
+            return res.status(400).json(response);
+        }
+        interface UserAuthenticationResponse {
+            status: boolean;
+            user: User | null;
+            message: string;
+        }
 
-    const validationStatus: UserAuthenticationResponse = await User.AuthenticateUserByEmailAndPassword(email, password);
-    /**
-     * if status is true the create accesstoken
-     */
-    if (validationStatus.status === false || validationStatus.user === null) {
-        return res.status(400).json({
-            status: 400,
-            message: validationStatus.message,
+        const validationStatus: UserAuthenticationResponse = await User.AuthenticateUserByEmailAndPassword(email, password);
+        /**
+         * if status is true the create accesstoken
+         */
+        if (validationStatus.status === false || validationStatus.user === null) {
+            return res.status(400).json({
+                status: 400,
+                message: validationStatus.message,
+                token: null,
+                user: null
+            });
+        }
+
+        interface UserDataResponse {
+            id: number,
+            email: string,
+            username: string
+        }
+        const userData: UserDataResponse = validationStatus.user;
+        /**
+         * create access token and refresh token
+         */
+        const authenticationObject: Authentication = Authentication.getInstance();
+        const accessToken = authenticationObject.Generate_Access_Token(userData.id);
+        const refreshToken = authenticationObject.Generate_Refresh_Token(userData.id);
+        if (!accessToken.token || !refreshToken.refreshToken) {
+            return res.status(400).json({
+                status: 400,
+                message: 'Somthing went wrong could not generate access token and refresh token',
+                token: null,
+                user: null
+            });
+        }
+
+        res.cookie("refreshToken", refreshToken.refreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: process.env.NODE_ENVIRONMENT == "PRODUCTION",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+
+        return res.status(200).json({
+            status: 200,
+            message: 'Login SuccessFully',
+            token: accessToken.token,
+            user: userData
+        });
+    } catch (error) {
+        return res.status(200).json({
+            status: 200,
+            message: ErrorHandler.getMessage(error),
             token: null,
             user: null
         });
     }
 
-    interface UserDataResponse {
-        id: number,
-        email: string,
-        username: string
-    }
-    const userData: UserDataResponse = validationStatus.user;
-    /**
-     * create access token and refresh token
-     */
-    const authenticationObject: Authentication = Authentication.getInstance();
-    const accessToken = authenticationObject.Generate_Access_Token(userData.id);
-    const refreshToken = authenticationObject.Generate_Refresh_Token(userData.id);
-    if (!accessToken.token || !refreshToken.refreshToken) {
-        return res.status(400).json({
-            status: 400,
-            message: 'Somthing went wrong could not generate access token and refresh token',
-            token: null,
-            user: null
-        });
-    }
-
-    res.cookie("refreshToken", refreshToken.refreshToken, {
-        httpOnly: true,
-        sameSite: 'lax',
-        secure: process.env.NODE_ENVIRONMENT == "PRODUCTION",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-
-
-    return res.status(200).json({
-        status: 200,
-        message: 'Login SuccessFully',
-        token: accessToken.token,
-        user: userData
-    })
 }
 
 /**
@@ -153,7 +164,7 @@ export const RegisterAction = async (req: Request, res: Response) => {
     catch (error) {
         return res.status(400).json({
             status: 400,
-            message: error || 'User creation failed'
+            message: ErrorHandler.getMessage(error),
         });
     }
 }
@@ -243,7 +254,7 @@ export const LogoutAction = async (req: Request, res: Response) => {
     } catch (error) {
         return res.status(200).json({
             status: 500,
-            message: 'Somting went wrong'
+            message: ErrorHandler.getMessage(error),
         })
     }
 
