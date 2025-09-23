86f91ae0efad7f257b976e0ef26ebfbc3ce4eee2
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-23 23:03:50 +0530

    channel parent route

--- services/api-gateway/src/router/index.ts
+++ services/api-gateway/src/router/index.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 const router: Router = Router();
 import authRoute from "./authRoute";
 import serviceRoute from './serviceRoute';
-
+import channeRoute from './channelRoute';
 
 router.use('/auth', authRoute);
 router.use('/service', serviceRoute);
+router.use('/channel', channeRoute)
 
 export default router;
