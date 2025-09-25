d63dda80b2b941a95dac136107044c93a0afd341
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-26 00:03:50 +0530

    memberships model created

---
+++ services/api-gateway/src/models/Memberships.ts
@@ -0,0 +1,73 @@
+import sequelize from "../config/sqldb";
+import { DataTypes, Model } from "sequelize";
+
+
+class Memberships extends Model {
+    public id!: number;
+    public user_id!: number;
+    public organization_id!: number;
+    public role!: string;
+    public created_at!: Date
+    public updated_at!: Date
+}
+
+Memberships.init({
+    id: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    },
+    user_id: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    organization_id: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    sequelize,
+    tableName: 'memberships',
+    modelName: 'Memberships',
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    hooks: {
+        beforeCreate: (memberships: Memberships) => {
+            memberships.created_at = new Date();
+            memberships.updated_at = new Date();
+        },
+        beforeUpdate: (memberships: Memberships) => {
+            memberships.updated_at = new Date();
+        }
+    }
+});
+
+
+export default Memberships;
