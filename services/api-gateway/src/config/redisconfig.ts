b4e49bac94d30f6031cc2c9913d92f752795b7c1
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-30 18:45:50 +0530

    configure redis

---
+++ services/api-gateway/src/config/redisconfig.ts
@@ -0,0 +1,18 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+    url: process.env.REDIS_URI
+});
+
+
+redisClient.on("connect", () => console.log("✅ Redis connected"));
+redisClient.on("error", (err: any) => console.error("❌ Redis error", err));
+
+export const connectRedis = async () => {
+    if (!redisClient.isOpen) {
+        await redisClient.connect();
+    }
+    return redisClient;
+}
+
+export default redisClient;
---
+++ services/api-gateway/src/service/Cache.ts
@@ -0,0 +1,28 @@
+import { connectRedis } from "../config/redisconfig";
+export async function setCache(key: string, ttl: number, value: any) {
+    try {
+        const redis = await connectRedis();
+        const stringValue =
+            typeof value === "string" ? value : JSON.stringify(value);
+
+        await redis.set(key, stringValue, { EX: ttl });
+    } catch (err: unknown) {
+        console.error("Error while caching data:", err);
+    }
+}
+
+export async function getCache<T>(key: string): Promise<T | null> {
+    try {
+        const redis = await connectRedis();
+        const data = await redis.get(key);
+        if (!data) return null;
+        try {
+            return JSON.parse(data) as T;
+        } catch {
+            return data as unknown as T;
+        }
+    } catch (err: unknown) {
+        console.error("Error while getting cache:", err);
+        return null;
+    }
+}
