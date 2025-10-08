d1075ef4838324ac7718b8a83c07f80242fb749f
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-10-08 19:23:23 +0530

    server code to connect socket

--- services/socket-gateway/src/index.ts
+++ services/socket-gateway/src/index.ts
@@ -1,15 +1,27 @@
-import express, { Request, Response } from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const app = express();
-const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cleanupEmptyChildNamespaces: true,
+    cors: {
+        origin: ["http://localhost:5173/"],
+        allowedHeaders: ["authorization"],
+        credentials: true,
+    },
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).send('API Gateway is running');
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('Connect to socket id: ' + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("🔴 User disconnected:", socket.id);
+    });
+})
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+httpServer.listen(PORT, () => {
     console.log(`Scoket Gateway is running on http://localhost:${PORT}`);
 });
