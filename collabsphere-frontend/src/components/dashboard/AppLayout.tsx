e0cff68e1e65893a189d2468c09c110d3c9f9f00
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-10-10 23:06:07 +0530

    connect when user log in

--- collabsphere-frontend/src/components/dashboard/AppLayout.tsx
+++ collabsphere-frontend/src/components/dashboard/AppLayout.tsx
@@ -2,7 +2,27 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../header/Navigation";
 import ServiceMenu from "../serviceMenu/ServiceMenu";
+import { connectSocket, disconnectSocket } from "../../features/ChatBoxSlice/SocketConnect";
+import { useAppSelector, useAppDispatch } from "../customHooks/reduxCustomHook";
+import { useEffect } from "react";
+import useGetUserCredentials from "../customHooks/getUserCredentials";
 export default function AppLayout() {
+    const { accessToken } = useGetUserCredentials();
+    const dispatch = useAppDispatch();
+    const { isConnected } = useAppSelector(state => state.SocketConnectReducer);
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted && isConnected === false) {
+            dispatch(connectSocket({ accessToken: accessToken }));
+        }
+        // Cleanup function to disconnect socket when component unmounts
+        return () => {
+            isMounted = false;
+            if (isConnected) {
+                dispatch(disconnectSocket());
+            }
+        };
+    }, [isConnected, accessToken, dispatch]);
     return (
         <div className="flex w-full h-full bg-[#ebecef]">
             <div className="w-[25%] h-screen bg-[#1f2937] text-white">
